475a804e1866127215e15b3c7cd040ed4596a67c
linchen <clin366@gatech.edu>
2020-01-20 15:54:38 -0500

    predict per city

--- src/evaluation/predict_model.py
+++ src/evaluation/predict_model.py
@@ -84,8 +84,8 @@ def extract_file(config_path, test_data_path):
         pred_class, pred_score = model.predict(x_test)
         result_scores = result_stat(y_test, pred_class, pred_score)
         print(result_scores)
-        result_scores = [model_type, feature_pars['feature'], feature_pars['is_two_branch'], search_lag] + \
-                        result_scores
+        result_scores = [city, model_type, feature_pars['feature'], feature_pars['is_two_branch'],
+                         search_lag] + result_scores
         record_pd = write_report(result_scores, record_pd, row_count)
         row_count += 1
 
